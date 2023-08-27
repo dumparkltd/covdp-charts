dee3c8a830de2a29b0e818966e08c67fd854de56
tmfrnz <timo@dumpark.com>
2023-08-27 13:40:53 +0200

    add custom fonts

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,6 +1,65 @@
 import { createGlobalStyle } from 'styled-components';
 
+import ABCMonumentGroteskBoldWOFF from './fonts/ABCMonumentGrotesk-Bold-Trial.woff';
+import ABCMonumentGroteskBoldOTF from './fonts/ABCMonumentGrotesk-Bold-Trial.otf';
+import ABCMonumentGroteskRegularWOFF from './fonts/ABCMonumentGrotesk-Regular-Trial.woff';
+import ABCMonumentGroteskRegularOTF from './fonts/ABCMonumentGrotesk-Regular-Trial.otf';
+import ABCMonumentGroteskMonoBoldWOFF from './fonts/ABCMonumentGroteskMono-Bold-Trial.woff';
+import ABCMonumentGroteskMonoBoldOTF from './fonts/ABCMonumentGroteskMono-Bold-Trial.otf';
+import ABCMonumentGroteskMonoRegularWOFF from './fonts/ABCMonumentGroteskMono-Regular-Trial.woff';
+import ABCMonumentGroteskMonoRegularOTF from './fonts/ABCMonumentGroteskMono-Regular-Trial.otf';
+import PPFragmentSerifThinOTF from './fonts/PPFragment-SerifThin.otf';
+import PPFragmentSerifThinWOFF from './fonts/PPFragment-SerifThin.woff';
+import PPFragmentSerifThinTTF from './fonts/PPFragment-SerifThin.ttf';
+import PPFragmentSerifThinItalicOTF from './fonts/PPFragment-SerifThinItalic.otf';
+import PPFragmentSerifThinItalicWOFF from './fonts/PPFragment-SerifThinItalic.woff';
+import PPFragmentSerifThinItalicTTF from './fonts/PPFragment-SerifThinItalic.ttf';
+
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'ABCMonument';
+    src: url(${ABCMonumentGroteskRegularWOFF}) format('woff'),
+      url(${ABCMonumentGroteskRegularOTF}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'ABCMonumentBold';
+    src: url(${ABCMonumentGroteskBoldWOFF}) format('woff'),
+      url(${ABCMonumentGroteskBoldOTF}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'ABCMonumentMono';
+    src: url(${ABCMonumentGroteskMonoRegularWOFF}) format('woff'),
+      url(${ABCMonumentGroteskMonoRegularOTF}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'ABCMonumentMonoBold';
+    src: url(${ABCMonumentGroteskMonoBoldWOFF}) format('woff'),
+      url(${ABCMonumentGroteskMonoBoldOTF}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'PPFragment';
+    src: url(${PPFragmentSerifThinWOFF}) format('woff'),
+      url(${PPFragmentSerifThinTTF}) format('truetype'),
+      url(${PPFragmentSerifThinOTF}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'PPFragmentItalic';
+    src: url(${PPFragmentSerifThinItalicWOFF}) format('woff'),
+      url(${PPFragmentSerifThinItalicTTF}) format('truetype'),
+      url(${PPFragmentSerifThinItalicOTF}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
   html,
   body {
     height: 100%;
@@ -9,7 +68,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-family: 'ABCMonument', 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
 
   // body.fontLoaded {
